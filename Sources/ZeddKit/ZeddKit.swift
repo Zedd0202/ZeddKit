951851e41f1dfb4e59c9281f3cf76022615e2234
Zedd <zedd.0202@kakaocorp.com>
2020-07-02 18:40:02 +0900

    - 이니셜라이저 추가.

--- Sources/ZeddKit/ZeddKit.swift
+++ Sources/ZeddKit/ZeddKit.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 public struct ZeddKit {
+    public init() {}
     public var text = "Hello, World! Zedd"
     public let image = UIImage(named: "wwdc", in: Bundle.module, with: .none)
 }
