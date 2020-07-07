c51a6ebbb210fbeacc85c1b22e41e28c513c3dcb
Zedd <zedd.0202@kakaocorp.com>
2020-07-07 11:01:21 +0900

    - copy추가.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ZeddKit",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("Login.storyboard")]),
         .testTarget(
             name: "ZeddKitTests",
             dependencies: ["ZeddKit"]),
